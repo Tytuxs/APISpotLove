3fdab14263015d000d4f20e97ae443d5b5c9bc5f
Tytus <60603658+Tytuxs@users.noreply.github.com>
2025-01-28 13:27:04 +0100

    init

---
+++ src/main/java/be/apispotlove/ApiSpotLoveApplication.java
@@ -0,0 +1,13 @@
+package be.apispotlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpotLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpotLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/apispotlove/ApiSpotLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package be.apispotlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpotLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
